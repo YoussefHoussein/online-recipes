48013027581ad6c2b52d9e2d466094a8796d81bc
unknown <youssefhoussein701@gmail.com>
2023-08-19 21:01:32 +0300

    backend <> add the migrations and the jwt

---
+++ backend/database/migrations/2023_08_19_171406_create-recipes-table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cuisine');
+            $table->string('ingredients');
+            $table->Integer('user_id');
+            $table->timestamps();
+        });
+
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->Integer('recipe_id');
+            $table->timestamps();
+        });
+
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->Integer('recipe_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('recipes');
+        Schema::dropIfExists('images');
+        Schema::dropIfExists('plans');
+    }
+};
---
+++ backend/database/migrations/2023_08_19_171724_create-pivot-tables.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->Integer('recipe_id');
+            $table->Integer('user_id');
+            $table->timestamps();
+        });
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->Integer('recipe_id');
+            $table->Integer('user_id');
+            $table->timestamps();
+        });
+
+        Schema::create('recipes_in_lists', function (Blueprint $table) {
+            $table->id();
+            $table->Integer('recipe_id');
+            $table->Integer('shopping_list_id');
+            $table->timestamps();
+        });
+
+        Schema::create('Shopping_lists', function (Blueprint $table) {
+            $table->id();
+            $table->Integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
