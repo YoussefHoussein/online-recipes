9c925e5c93071f404c01ba034bab97ca5bd23182
unknown <youssefhoussein701@gmail.com>
2023-08-21 16:04:24 +0300

    frontend <> add the carousel for images in recipeCard

--- frontend/src/components/home/recipeCard/index.jsx
+++ frontend/src/components/home/recipeCard/index.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import './style.css'
 import {AiFillHeart, AiFillMessage, AiOutlineShareAlt} from 'react-icons/ai'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import image from '../../../assessts/loginImage.jpg'
+
 const RecipeCard = () => {
   return (
     <div className='recipe-card flex center'>
         <div className="recipe-container flex column spaceBetween">
-            <div className="images"></div>
+            <div className="images">
+                <Carousel>
+                    <div>
+                        <img src={image} alt="Image 1" />
+                    </div>
+                    <div>
+                        <img src={image} alt="Image 2" />
+                    </div>
+                    <div>
+                        <img src={image} alt="Image 3" />
+                    </div>
+                </Carousel>
+            </div>
+            
             <h2>Name</h2>
             <h5>Cuisine</h5>
             <div>
                 ingredients: 
-                <ul>
+            <ul>
                 <li>rice</li>
-                <li>tomato</li>
+                
             </ul>
             </div>
             
